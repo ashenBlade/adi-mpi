a35a2bddca81039dd610d1437fc66f1357a9ddf9
ash.blade <ashen.blade.dev@gmail.com>
2022-09-11 19:33:27 +0300

    add vectors summarizing

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,11 +1,79 @@
 #include <iostream>
 #include <omp.h>
 #include <sstream>
+#include <unistd.h>
+#include <memory>
+#include <vector>
+#include <algorithm>
 
 #define BUFFER_SIZE 1000
+#define VECTORS_COUNT 2
+
+std::vector<int>* init_random_vectors(size_t vector_size, size_t vector_count = 2) {
+    auto vectors = new std::vector<int>[vector_count];
+    for (int i = 0; i < vector_count; ++i) {
+        vectors[i] = std::vector<int>(vector_size);
+    }
+
+    return vectors;
+}
+
+long sum_vectors_single(size_t vector_size, size_t vectors_count = VECTORS_COUNT) {
+    auto vectors = init_random_vectors(vector_size, vectors_count);
+    long total_sum = 0;
+
+    omp_set_nested(true);
+    for (int i = 0; i < vectors_count; ++i) {
+        auto vector = vectors[i];
+        int n;
+        for (int j : vector) {
+            n = j;
+        }
+
+        total_sum = n;
+    }
+
+    delete[] vectors;
+    return total_sum;
+}
+
+long sum_vectors_parallel(size_t vector_size, size_t vectors_count = VECTORS_COUNT) {
+    auto vectors = init_random_vectors(vector_size, vectors_count);
+    long total_sum = 0;
+
+    omp_set_nested(true);
+#pragma omp parallel for shared(vectors_count, vectors) reduction(+:total_sum) default(none)
+    for (int i = 0; i < vectors_count; ++i) {
+        auto vector = vectors[i];
+        int n;
+#pragma omp parallel for reduction(+:n) private(vector) default(none)
+        for (int j : vector) {
+            n = j;
+        }
+
+        total_sum = n;
+    }
+
+    delete[] vectors;
+    return total_sum;
+}
 
 
 int main(int argc, char** argv) {
+    auto start = omp_get_wtime();
+    int vectors_count = 100;
+    int vector_size = 100000;
+    auto result = sum_vectors_parallel(vector_size, vectors_count);
+    auto end = omp_get_wtime();
+    std::cout << "Execution time for parallel: " << (end - start) << std::endl;
+    
+    start = omp_get_wtime();
+    result = sum_vectors_single( vector_size, vectors_count);
+    end = omp_get_wtime();
+    std::cout << "Execution time for single: " << end - start << std::endl;
+}
+
+void rewritten_main(int argc, char** argv) {
     std::cout << "Processors count: " << omp_get_num_procs() << "\n";
     int a[BUFFER_SIZE],
             b[BUFFER_SIZE],
