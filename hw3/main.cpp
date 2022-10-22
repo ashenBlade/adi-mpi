1b8fa03428257cf2ae2ab30ff53cceda55ca5a9e
ash.blade <ashen.blade.dev@gmail.com>
2022-10-22 10:31:02 +0300

    add class task

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "mpi/mpi.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+    int numtask, rank;
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtask);
+
+    std::cout << "Hello, " + std::to_string(rank) + " " + std::to_string(numtask) + "\n";
+
+    MPI_Finalize();
 }
