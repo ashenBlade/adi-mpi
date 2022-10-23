33d48b94d315af0389d14005ab5ed3572d9df98e
ash.blade <ashen.blade.dev@gmail.com>
2022-10-23 09:53:00 +0300

    add matrix struct

---
+++ spline/matrix.h
@@ -0,0 +1,16 @@
+//
+// Created by ashblade on 23.10.22.
+//
+
+#ifndef SPLINE_MATRIX_H
+#define SPLINE_MATRIX_H
+
+struct matrix {
+    int* a;
+    int* b;
+    int* c;
+    int* f;
+    int length;
+} matrix_t, matrix_part_t;
+
+#endif //SPLINE_MATRIX_H
