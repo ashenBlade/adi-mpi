e015a315d90139ccff31e72a3219f38690ada30e
ash.blade <ashen.blade.dev@gmail.com>
2022-10-26 13:24:54 +0300

    add up/down ranks recievers

--- spline/main.c
+++ spline/main.c
@@ -7,7 +7,10 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    printf("%i %i\n", rank, size);
+    int up = rank == 0 ? MPI_PROC_NULL : rank - 1,
+            down = rank == size - 1 ? MPI_PROC_NULL : rank + 1;;
+
+    printf("%i %i\n", up, down);
 
     MPI_Finalize();
     return 0;
