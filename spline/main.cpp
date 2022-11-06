ea937c9dd0e65cd811995f33ea588d3db424e87b
ash.blade <ashen.blade.dev@gmail.com>
2022-11-06 12:11:39 +0300

    some fixes

--- spline/main.cpp
+++ spline/main.cpp
@@ -2,39 +2,16 @@
 #include <iostream>
 #include "mpi.h"
 
-
-#define INITIAL_MATRIX_VALUE 300
-#define TIME_DELTA 0.0001
-
-
-using matrix_t = double**;
-
-matrix_t createInitialMatrix(int rows, int columns) {
-    auto matrix = new double * [rows];
-    for (int row = 0; row < rows; ++row) {
-        auto column = new double[columns];
-        for (int i = 0; i < columns; ++i) {
-            column[i] = INITIAL_MATRIX_VALUE;
-        }
-        matrix[row] = column;
-    }
-    return matrix;
-}
-
-double getLambda(double x, double y) {
-    return 0.25 <= x && x <= 0.65 &&
-           0.1 <= y && y <= 0.25
-           ? 0.01
-           : 0.0001;
-}
+#define BETA_0 0
+#define BETA_N 1
 
 
 
 int main(int argc, char** argv) {
     double xMin = 0, xMax = 1;
     const int n = 10;
-    double step = (xMax - xMin) / n;
     const int MAX_SIZE = n + 1;
+    double step = (xMax - xMin) / n;
     double A[MAX_SIZE],
             B[MAX_SIZE],
             C[MAX_SIZE],
@@ -55,23 +32,24 @@ int main(int argc, char** argv) {
         C[i] = D[i] + D[i + 1];
         f[i] = -F[i] * (step * step);
     }
+
     B[MAX_SIZE - 1] = A[0] = 0;
 
     double alpha[MAX_SIZE],
             beta[MAX_SIZE];
 
-    alpha[1] = 0; // хи 1
-    beta[1] = 1; // мю 1
-
+    alpha[0] = alpha[MAX_SIZE - 1] = 0;
+    beta[0] = BETA_0;
     for (int i = 1; i < MAX_SIZE - 1; ++i) {
-        alpha[i + 1] =  B[i] / (C[i] - alpha[i] * A[i]);
-        beta[i + 1] = (A[i] * beta[i] + F[i]) / (C[i] - alpha[i]  * A[i]);
+        alpha[i] =  -B[i] / (C[i] + alpha[i - 1] * A[i]);
+        beta[i] = (A[i] * beta[i - 1] + F[i]) / (C[i] - alpha[i - 1]  * A[i]);
     }
+    beta[MAX_SIZE - 1] = BETA_N;
 
     double y[MAX_SIZE];
     y[MAX_SIZE - 1] = 1;
 
-    for (int i = MAX_SIZE - 2; i >= 0; i--) {
+    for (int i = MAX_SIZE - 2; i >= 0; --i) {
         y[i] = alpha[i + 1] * y[i + 1] + beta[i + 1];
         std::cout << "y[" << i << "] = " << y[i] << "\n";
     }
