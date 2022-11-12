66459c74c7c93b4c06add5cc5c48032557ae4918
ash.blade <ashen.blade.dev@gmail.com>
2022-11-12 11:02:19 +0300

    сделал ебаную прогонку

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,111 +1,51 @@
+#include <algorithm>
 #include <iostream>
-#include <omp.h>
-#include <sstream>
-#include <unistd.h>
-#include <memory>
-#include <vector>
-#include "vectors_summarizing.h"
-#include "matrix.h"
-#include <string>
 
-using exec_time = double;
+template <class T>
+void quickSort(T* a, const long n) {
+    long i = 0, j = n-1;
+    T pivot = a[n / 2];
 
-struct ThreadTestResult {
-    int threadsCount;
-    exec_time standard;
-    exec_time band;
-    exec_time block;
-};
-
-struct TestResult {
-    int size;
-    double sequential;
-    std::vector<ThreadTestResult> parallelResults;
-};
-
-std::vector<TestResult>* makeTests();
-
-void test_parallel_mode();
-
-std::vector<TestResult> *makeTests() {
-    auto vector = new std::vector<TestResult>();
-    auto matrixSizes = {500, 1000, 1500, 2000, 2500, 3000};
-    auto threadsCounts = {2, 4, 6, 8};
-    for (const auto &size: matrixSizes) {
-        auto result = TestResult();
-        result.size = size;
-        auto left = std::unique_ptr<Matrix>(Matrix::createRandomMatrix(size));
-        auto right = std::unique_ptr<Matrix>(Matrix::createRandomMatrix(size));
-        result.sequential = left->multiplySequential(*right);
-        for (const auto &threads: threadsCounts) {
-            auto threadResult = ThreadTestResult();
-            threadResult.threadsCount = threads;
-            threadResult.standard = left->multiplyStandard(*right, threads);
-            threadResult.band = left->multiplyBand(*right, threads);
-            threadResult.block = left->multiplyBlock(*right, threads);
-            result.parallelResults.push_back(threadResult);
-            std::cerr << "Done: " << size << " size, " << threads << " threads\n";
+    do {
+        while (a[i] < pivot) {
+            i++;
         }
-        vector->push_back(result);
-    }
-    return vector;
-}
+        while (a[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            std::swap(a[i], a[j]);
+            i++; j--;
+        }
+    } while (i <= j);
 
-void showResults(const std::vector<TestResult>& results) {
-    for (const auto &result: results) {
-        std::cout << "---------------------------------------------\n";
-        std::cout << "Size: " << result.size << "\n\n";
-        std::cout << "Sequential: " << result.sequential << "\n\n";
-        for (const auto &parallelResult: result.parallelResults) {
-            std::cout << "Threads: " << parallelResult.threadsCount << "\n";
-            std::cout << "\tStandard:\n\t\tTime: " << parallelResult.standard << "\n\t\t" << "Acceleration: " <<  result.sequential / parallelResult.standard << "\n";
-            std::cout << "\tBand:\n\t\tTime: " << parallelResult.band << "\n\t\t" << "Acceleration: " <<  result.sequential / parallelResult.band << "\n";
-            std::cout << "\tBlock:\n\t\tTime: " << parallelResult.block << "\n\t\t" << "Acceleration: " <<  result.sequential / parallelResult.block << "\n\n";
+#pragma omp task shared(a, j) default(none)
+    {
+        if (j > 0) {
+            quickSort(a, j+1);
         }
     }
+#pragma omp task shared(a, i, n) default(none)
+    {
+        if (n > i) {
+            quickSort(a + i, n - i);
+        }
+    }
+#pragma omp taskwait
 }
-
-
 
 int main(int argc, char** argv) {
-    int n;
-#pragma omp parallel private(n)
-    {
-        n = 1;
-#pragma omp master
-        {
-            n = 2;
-        };
-        std::cout << "First n: " + std::to_string(n) + '\n';
-#pragma omp barrier
-#pragma omp master
-        {
-            n = 3;
-        };
-        std::cout << "Second n: " + std::to_string(n) + '\n';
+    const int size = 100;
+    int* array = new int[size];
+    for (int i = size - 1; i >= 0; --i) {
+        array[i] = i;
     }
-}
-
-void test_parallel_mode() {
-    if (omp_in_parallel()) {
-        std::cout << "Parallel\n";
-    } else {
-        std::cout << "Sequential\n";
+    quickSort(array, size);
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
     }
-#pragma omp parallel
-    {
-#pragma omp master
-        {
-            if (omp_in_parallel()) {
-                std::cout << "Parallel\n";
-            } else {
-                std::cout << "Sequential\n";
-            }
-        }
-    };
-
+    delete[] array;
 }
-
 
 
 
--- spline/main.cpp
+++ spline/main.cpp
@@ -1,57 +1,65 @@
 #include <cstdio>
 #include <iostream>
 #include "mpi.h"
-
-#define BETA_0 0
-#define BETA_N 1
-
-
+#include <algorithm>
+#include "cmath"
 
 int main(int argc, char** argv) {
-    double xMin = 0, xMax = 1;
     const int n = 10;
-    const int MAX_SIZE = n + 1;
-    double step = (xMax - xMin) / n;
-    double A[MAX_SIZE],
-            B[MAX_SIZE],
-            C[MAX_SIZE],
-            D[MAX_SIZE],
-            f[MAX_SIZE],
-            F[MAX_SIZE],
-            x[MAX_SIZE];
+    const int N = n + 1;
+    const double xMin = 0,
+                xMax = 3.1415 / 2;
+    const double xStep = (xMax - xMin) / n;
 
-    for (int i = 0; i < MAX_SIZE; ++i) {
-        x[i] = xMin + step * i;
-        D[i] = x[i];
-        F[i] = 4 * x[i];
+    double A[N],
+            B[N],
+            C[N],
+            f[N],
+            F[N],
+            y[N];
+
+    double x[N], d[N];
+    for (int i = 0; i < N; ++i) {
+        x[i] = i * xStep;
+        d[i] = 1;
+        F[i] = -std::sin(x[i]);
+        f[i] = -F[i] * xStep * xStep;
     }
 
-    for (int i = 0; i < MAX_SIZE; ++i) {
-        A[i] = D[i];
-        B[i] = D[i + 1];
-        C[i] = D[i] + D[i + 1];
-        f[i] = -F[i] * (step * step);
+    for (int i = 0; i < N; ++i) {
+        A[i] = d[i];
+        B[i] = d[i + 1];
+        C[i] = d[i] + d[i + 1];
     }
 
-    B[MAX_SIZE - 1] = A[0] = 0;
+    A[0] = B[N - 1] = 0;
 
-    double alpha[MAX_SIZE],
-            beta[MAX_SIZE];
+    double alpha[N],
+            beta[N];
 
-    alpha[0] = alpha[MAX_SIZE - 1] = 0;
-    beta[0] = BETA_0;
-    for (int i = 1; i < MAX_SIZE - 1; ++i) {
-        alpha[i] =  -B[i] / (C[i] + alpha[i - 1] * A[i]);
-        beta[i] = (A[i] * beta[i - 1] + F[i]) / (C[i] - alpha[i - 1]  * A[i]);
+//    alpha[0] = -B[0] / C[0];
+//    beta[0] = F[0] / C[0];
+    alpha[0] = 0;
+    for (int i = 0; i < N - 1; ++i) {
+        alpha[i + 1] =
+                B[i]
+                /
+                (C[i] - A[i] * alpha[i]);
+        beta[i + 1] =
+                (f[i] + A[i] * beta[i])
+                /
+                (C[i] - A[i] * alpha[i]);
     }
-    beta[MAX_SIZE - 1] = BETA_N;
 
-    double y[MAX_SIZE];
-    y[MAX_SIZE - 1] = 1;
+    y[0] = 0;
+    y[N - 1] = 1;
 
-    for (int i = MAX_SIZE - 2; i >= 0; --i) {
+    for (int i = N - 2; i >= 0; --i) {
         y[i] = alpha[i + 1] * y[i + 1] + beta[i + 1];
-        std::cout << "y[" << i << "] = " << y[i] << "\n";
+    }
+
+    for (int i = 0; i < N; ++i) {
+        std::cout << "y[" << i << "] = " << y[i] << '\n';
     }
     return 0;
 }
