68c6613ecdff5941fbbbf894c571fc78d38afb1b
ash.blade <ashen.blade.dev@gmail.com>
2022-11-13 12:18:20 +0300

    add additional functions

--- spline/main.cpp
+++ spline/main.cpp
@@ -1,48 +1,46 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <vector>
 #include "cmath"
 
-#define X_PARTITIONS_COUNT (10)
-#define X_MIN (0)
-#define X_MAX (1)
+#define DEFAULT_PARTITIONS_COUNT 10
 
-#define Y_PARTITIONS_COUNT (10)
-#define Y_MIN (0)
+#define X_PARTITIONS_COUNT (DEFAULT_PARTITIONS_COUNT)
+#define X_MIN (0.0)
+#define X_MAX (1.0)
+#define X_STEP ((X_MAX - X_MIN) / X_PARTITIONS_COUNT)
+
+#define Y_PARTITIONS_COUNT (DEFAULT_PARTITIONS_COUNT)
+#define Y_MIN (0.0)
 #define Y_MAX (0.5)
+#define Y_STEP ((Y_MAX - Y_MIN) / Y_PARTITIONS_COUNT)
+
+#define T_START 0
+#define T_END 10
 
 double getLambda(double x, double y) {
     return 0.25 <= x && x <= 0.65 &&
     0.1 <= y && y <= 0.25 ? 0.01 : 0.0001;
 }
 
-std::vector<double>* solveThomas(int partitionsCount, double yMin, double yMax) {
+/// Решаем прогонку в 1 измерении
+std::vector<double>* solveThomas(const int partitionsCount,
+                                 const double yMin,
+                                 const double yMax,
+                                 const std::vector<double>& lambda,
+                                 const std::vector<double>& f) {
     const int N = partitionsCount;
-    const double step = (yMax - yMin) / partitionsCount;
-
-    double A[N],
-            B[N],
-            C[N],
-            f[N],
-            F[N];
+//    const double step = (yMax - yMin) / partitionsCount;
 
     auto Y = new std::vector<double>(N);
     auto& y = *Y;
 
-    double x[N],
-            D[N]; // В будущем будет значениями лямбда в точке (x, y)
-
+    double A[N], B[N], C[N];
     for (int i = 0; i < N; ++i) {
-        x[i] = i * step;
-        D[i] = 1;
-        F[i] = -std::sin(x[i]);
-        f[i] = -F[i] * step * step;
-    }
-
-    for (int i = 0; i < N; ++i) {
-        A[i] = D[i];
-        B[i] = D[i + 1];
-        C[i] = D[i] + D[i + 1];
+        A[i] = lambda[i];
+        B[i] = lambda[i + 1];
+        C[i] = lambda[i] + lambda[i + 1];
     }
 
     A[0] = B[N - 1] = 0;
@@ -51,14 +49,8 @@ std::vector<double>* solveThomas(int partitionsCount, double yMin, double yMax) 
             beta[N];
     alpha[0] = 0;
     for (int i = 0; i < N - 1; ++i) {
-        alpha[i + 1] =
-                B[i]
-                /
-                (C[i] - A[i] * alpha[i]);
-        beta[i + 1] =
-                (f[i] + A[i] * beta[i])
-                /
-                (C[i] - A[i] * alpha[i]);
+        alpha[i + 1] = B[i] / (C[i] - A[i] * alpha[i]);
+        beta[i + 1] = (f[i] + A[i] * beta[i]) / (C[i] - A[i] * alpha[i]);
     }
 
     // Граничные условия
@@ -72,13 +64,34 @@ std::vector<double>* solveThomas(int partitionsCount, double yMin, double yMax) 
     return Y;
 }
 
+std::vector<std::vector<double>*>* initField(int rowsCount, int columnsCount) {
+    auto rows = new std::vector<std::vector<double>*>();
+    for (int i = 0; i < rowsCount; ++i) {
+        auto column = new std::vector<double>();
+        for (int j = 0; j < columnsCount; ++j) {
+            column->push_back(300);
+        }
+        rows->push_back(column);
+    }
+    return rows;
+}
 
 
 int main(int argc, char** argv) {
-    auto y = solveThomas(X_PARTITIONS_COUNT, X_MIN, X_MAX);
+    std::vector<double> lambda;
+    std::vector<double> F;
 
+    std::vector<double> f;
+    for (int i = 0; i < X_PARTITIONS_COUNT; ++i) {
+        lambda.push_back(getLambda(0, 0));
+        f.push_back(300);
+    }
+    f[0] = 600;
+    f[X_PARTITIONS_COUNT - 1] = 1200;
+
+    auto y = solveThomas(X_PARTITIONS_COUNT, X_MIN, X_MAX, lambda, f);
     for (int i = 0; i < y->size(); ++i) {
-        std::cout << "y[" << i << "] = " << y->operator[](i) << std::endl;
+        std::cout << y->operator[](i) << std::endl;
     }
 
     delete y;
