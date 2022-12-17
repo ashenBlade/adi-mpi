bbcc7bb7804215e3c96a48dab86660fb67bcc509
ash.blade <ashen.blade.dev@gmail.com>
2022-12-17 13:46:17 +0300

    fuck yeah

--- spline/main.cpp
+++ spline/main.cpp
@@ -40,11 +40,11 @@ double avg(double left, double right) {
     return (left + right) / 2;
 }
 
-/// lambda должна быть на 2 больше, чем T, чтобы корректно проинициализировать T
+/// lambda должна быть на 2 больше, чем F, чтобы корректно проинициализировать F
 /// т.к. там используются средние значения
-/// len(T) = length
+/// len(F) = length
 /// len(lambda) = length
-void solveThomas(const double* T,
+void solveThomas(const double* F,
                     const double* lambda,
                     const int length,
                     const double min,
@@ -66,14 +66,13 @@ void solveThomas(const double* T,
     beta[0] = min;
     beta[length - 1] = max;
 
-    for (int i = 0; i < length - 1; ++i) {
-        alpha[i + 1] = B[i] / (C[i] - A[i] * alpha[i]);
-        beta[i + 1] = (T[i] + A[i] * beta[i]) / (C[i] - A[i] * alpha[i]);
+    for (int i = 0; i < length - 2; ++i) {
+        alpha[i + 1] = - B[i + 1] / (C[i + 1] + A[i + 1] * alpha[i]);
+        beta[i + 1] = (F[i + 1] - A[i + 1] * beta[i]) / (C[i + 1] + A[i + 1] * alpha[i]);
     }
 
-    // TODO: найти чему равно начальное (конечное) значение
     y[length - 1] = max;
-    y[0] = min;
+//    y[0] = min;
     for (int i = length - 2; i >= 0; i--) {
         y[i] = alpha[i] * y[i + 1] + beta[i];
     }
@@ -91,39 +90,16 @@ void restoreValues(const double* y,
                    const double timeStep,
                    double* T) {
     for (int i = 1; i < size - 1; ++i) {
-        double lambdaPlusHalf = avg(lambda[i + 1], lambda[i + 2]) / 2;
-        double lambdaMinusHalf = avg(lambda[i + 1], lambda[i]) / 2;
-        const auto value = y[i] / timeStep + (lambdaPlusHalf * (y[i + 1] - y[i]) - lambdaMinusHalf * (y[i] - y[i - 1])) / (step * step);
+        double lambdaPlusHalf = avg(lambda[i], lambda[i + 1]);
+        double lambdaMinusHalf = avg(lambda[i], lambda[i - 1]);
+        const auto value = y[i] / timeStep + (lambdaPlusHalf * (y[i + 1] - y[i]) - lambdaMinusHalf * (y[i] - y[i - 1])) / (2 * step * step);
         T[i] = value;
     }
 }
 
-
-std::vector<std::vector<double>*>* initField(int rowsCount, int columnsCount) {
-    auto rows = new std::vector<std::vector<double>*>();
-    for (int i = 0; i < rowsCount; ++i) {
-        auto column = new std::vector<double>();
-        for (int j = 0; j < columnsCount; ++j) {
-            column->push_back(300);
-        }
-        rows->push_back(column);
-    }
-    return rows;
-}
-
-void logMatrix(double** matrix, int size) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            std::cout << matrix[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
-}
-
-
 int main(int argc, char** argv) {
     const int size = 10;
-    const double timeStep = 0.001;
+    const double timeStep = 0.01;
     const double tLeft = 600;
     const double tRight = 1200;
     const double xMin = 0;
@@ -137,76 +113,75 @@ int main(int argc, char** argv) {
     double temperatureByX[size][size];
     double lambdaByX[size][size];
     // F
-    double TByX[size][size];
+    double FByX[size][size];
     for (int i = 0; i < size; ++i) {
         auto x = i * xStep;
         for (int j = 0; j < size; ++j) {
             auto y = j * yStep;
             lambdaByX[i][j] = getLambda(x, y);
             temperatureByX[i][j] = 300;
-            TByX[i][j] = 0;
+            FByX[i][j] = 0;
         }
     }
 
     for (int i = 0; i < size; ++i) {
-        restoreValues(temperatureByX[i], lambdaByX[i], size, xStep, timeStep, TByX[i]);
+        restoreValues(temperatureByX[i], lambdaByX[i], size, xStep, timeStep, FByX[i]);
     }
-    const int iterations = 10;
 
     // Меняю строки и столбцы
     double temperatureByY[size][size];
-    double TByY[size][size];
+    double FByY[size][size];
     double lambdaByY[size][size];
 
     for (int y = 0; y < size; ++y) {
         for (int x = 0; x < size; ++x) {
             temperatureByY[y][x] = temperatureByX[x][y];
-            TByY[y][x] = TByX[x][y];
+            FByY[y][x] = FByX[x][y];
             lambdaByY[y][x] = lambdaByX[x][y];
         }
     }
 
+
+    const int iterations = 10;
     for (int t = 0; t < iterations; ++t) {
         // Вычисляю yn+1/2
         for (int x = 0; x < size; ++x) {
-            solveThomas(TByX[x], lambdaByX[x], size, tLeft, tRight, xStep, timeStep, temperatureByY[x]);
+            solveThomas(FByX[x], lambdaByX[x], size, tLeft, tRight, xStep, timeStep, temperatureByX[x]);
         }
 
         // Вычисляю Fn+1/2
         for (int x = 0; x < size; ++x) {
-            restoreValues(temperatureByX[x], lambdaByX[x], size, xStep, timeStep, TByX[x]);
+            restoreValues(temperatureByX[x], lambdaByX[x], size, xStep, timeStep, FByX[x]);
         }
 
         for (int y = 0; y < size; ++y) {
             for (int x = 0; x < size; ++x) {
-                temperatureByY[y][x] = temperatureByX[x][y];
-                TByY[y][x] = TByX[x][y];
-                lambdaByY[y][x] = lambdaByX[x][y];
+                FByY[y][x] = FByX[x][y];
             }
         }
 
         // Вычисляю yn+1
         for (int y = 0; y < size; ++y) {
             double x = xMin + y * xStep;
-            solveThomas(TByY[y], lambdaByY[y], size, 600 * (1 + x), 600 * (1 + x * x * x), yStep, timeStep, temperatureByY[y]);
+            solveThomas(FByY[y], lambdaByY[y], size, 600 * (1 + x), 600 * (1 + x * x * x), yStep, timeStep, temperatureByY[y]);
         }
 
         // Вычисляю Fn+1
         for (int y = 0; y < size; ++y) {
-            restoreValues(temperatureByY[y], lambdaByY[y], size, yStep, timeStep, TByY[y]);
+            restoreValues(temperatureByY[y], lambdaByY[y], size, yStep, timeStep, FByY[y]);
         }
 
 
         for (int x = 0; x < size; ++x) {
             for (int y = 0; y < size; ++y) {
                 temperatureByX[x][y] = temperatureByY[y][x];
-                TByX[x][y] = TByY[y][x];
-                lambdaByX[x][y] = lambdaByY[y][x];
+                FByX[x][y] = FByY[y][x];
+//                lambdaByX[x][y] = lambdaByY[y][x];
             }
         }
 
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
+        for (int i = 1; i < size - 1; ++i) {
+            for (int j = 1; j < size - 1; ++j) {
                 std::cout << temperatureByX[i][j] << " ";
             }
             std::cout << "\n";
